05ef0c00558b9b7f299a0fcba33306b86c728dde
Jorge Cuevas <jcq012@gmail.com>
2019-04-03 00:48:01 -0400

    Grqphql types added

--- schema.js
+++ schema.js
@@ -1,5 +1,33 @@
 
 module.exports = `
+  type Team {
+    owner: User
+    members: [User!]!
+    channels: [Channel!]!
+  }
+
+  type Channel {
+    id: Int!
+    messages: [Message]
+    public: Boolean!
+    name: String!
+    users: [User!]!
+  }
+
+  type Message {
+    id: Int!
+    text: String!
+    user: User!
+    channel: Channel!
+  }
+
+  type User {
+    id: Int!
+    email: String!
+    username: String!
+    teams: [Team!]!
+  }
+
   type Query {
       hi: String
   }
